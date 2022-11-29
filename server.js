5833d920a412d6b19c1093fb9e92546fe3b4a863
wolfofwsb <aidansery@gmail.com>
2022-11-29 15:09:57 -0600

    created index page link to skills

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var methodOverride = require('method-override');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const methodOverride = require('method-override');
 
-var indexRouter = require('./routes/index');
-var skillsRouter = require('./routes/skills');
+const indexRouter = require('./routes/index');
+const skillsRouter = require('./routes/skills');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
